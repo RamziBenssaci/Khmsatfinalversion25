492b080519b488c6b6f15aa1681d214257e594f0
RamziBenssaci <129678228+RamziBenssaci@users.noreply.github.com>
2025-09-01 04:49:00 +0100

    Update ReportsList.tsx

--- src/pages/reports/ReportsList.tsx
+++ src/pages/reports/ReportsList.tsx
@@ -113,8 +113,8 @@ const exportToPDF = (data, filename) => {
               <td>${report.facilityName}</td>
               <td>${report.category}</td>
               <td>${report.deviceName}</td>
-              <td>${report.serialNumber || 'غير محدد'}</td>
-              <td>${report.problem_description || 'غير محدد'}</td>
+<td>${report.serial_number || 'غير محدد'}</td>
+<td>${report.problem_description || 'غير محدد'}</td>
               <td>
                 <span class="status ${
                   report.status === 'مفتوح' ? 'status-open' :
